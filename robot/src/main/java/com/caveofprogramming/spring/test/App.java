9782d0166d2ac93efcc9e84889c23730a8f40ecb
Zsolt Jakab <zsolt_jakab@epam.com>
2016-11-18 16:16:50 +0100

    spring web tutorial added

--- robot/src/main/java/com/caveofprogramming/spring/test/App.java
+++ robot/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -1,10 +1,12 @@
 package com.caveofprogramming.spring.test;
 
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 public class App {
 
@@ -14,12 +16,43 @@ public class App {
 		
 		OffersDAO offersDAO = (OffersDAO)context.getBean("offersDao");
 		
-		List<Offer> offers = offersDAO.getOffers();
-		
-		for (Offer offer : offers) {
-			System.out.println(offer);
+		try {
+			Offer albert = new Offer(6, "Alber", "albert@fake.com", "Web design");
+			Offer dave = new Offer("Dave", "dave@fake.com", "Happy coding");
+			Offer karen = new Offer("Karen", "karen@fake.com", "Coding Java");
+			
+			offersDAO.update(albert);
+			
+//			if (offersDAO.create(dave)) {
+//				System.out.println("Created offer object");
+//			}
+			
+			List<Offer> offers = new ArrayList<Offer>();
+			
+			offers.add(new Offer(3,"Steve", "steve@fake.com", "Code for cash"));
+			offers.add(new Offer(2,"Jack", "jack@fake.com", "Code from Bronx"));
+			
+			offersDAO.create(offers);
+			
+			offers = offersDAO.getOffers();
+			
+			for (Offer offer : offers) {
+				System.out.println(offer);
+			}	
+			
+			Offer offer = offersDAO.getOffer(2);
+			System.out.println("Should be Mike: " + offer);
+			
+			System.out.println(offersDAO.delete(88));
 		}
-		
+		catch (CannotGetJdbcConnectionException e) {
+			System.out.println("Cannot get database connection");
+		} 
+		catch (DataAccessException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());
+		}
+			
 		((ClassPathXmlApplicationContext)context).close();
 	}
 
